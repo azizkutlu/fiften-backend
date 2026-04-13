45cc183943fb1e9d164e40aa354d8a4ed4298f6f
Aziz Kutlu <azizkutlu792@gmail.com>
2026-04-13 16:53:58 +0300

    Remove unsupported Instagram OAuth scope

--- src/meta.js
+++ src/meta.js
@@ -19,8 +19,7 @@ export function buildInstagramLoginUrl() {
     [
       'instagram_business_basic',
       'instagram_business_manage_messages',
-      'instagram_business_manage_comments',
-      'instagram_business_content_publish'
+      'instagram_business_manage_comments'
     ].join(',')
   );
 
